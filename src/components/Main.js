546d5506519d0baa89f963467320461f2ce8db30
whosshazo <ryandeshazo1@gmail.com>
2022-06-02 05:09:16 -0400

    added RESUME to application

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ import pic03 from "../images/pic03.jpg";
 import pic04 from "../images/sk8spot.png";
 import pic05 from "../images/pic05.png";
 import pic06 from "../images/cocky-cocktails.png";
+import resume from "../assets/Ryan-DeShazo-Resume.pdf";
 
 class Main extends React.Component {
   render() {
@@ -133,19 +134,16 @@ class Main extends React.Component {
           }`}
           style={{ display: "none" }}
         >
-          <h2 className="major">About</h2>
-          <span className="image main">
-            <img src={pic03} alt="" />
-          </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
-          </p>
+          <h2 className="major">Resume</h2>
+          <button>
+            <a
+              href={resume}
+              download="RyanDeShazoResume"
+              style={{ borderBottom: "none" }}
+            >
+              Download.
+            </a>
+          </button>
           {close}
         </article>
 
